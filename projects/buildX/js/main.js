579e2088ce0251ebff3e13e2f8bfac59db0a5acc
NikitaFE <nikromongerredd@gmail.com>
2020-02-02 15:53:27 +0200

    add new project

---
+++ projects/buildX/js/main.js
@@ -0,0 +1,1 @@
+$(".header__button").click(function(){$(".header__burger").toggleClass("header__burger--active"),$(".nav").toggleClass("nav--hidden"),$(".header__contacts").toggleClass("header__contacts--hidden")}),$(".nav__button").click(function(){$(".nav__search").toggleClass("nav__search--hidden")});
---
+++ projects/buildX/js/swiper.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){new Swiper(".swiper-container",{slidesPerView:1,spaceBetween:0,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{1170:{slidesPerView:2,spaceBetween:30}}})});
